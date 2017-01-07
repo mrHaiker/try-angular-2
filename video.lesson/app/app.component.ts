c68fb3282f26bac77c7f9967f6aae877b299086f
Sergey <mr.haiker@mail.ru>
2017-01-07 23:51:42 +0200

    Video Lessons [Chapter 3 : Working With Components]

--- video.lesson/app/app.component.ts
+++ video.lesson/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import {MyComponent} from "./my-component";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello {{name}}</h1>
+  <my-component></my-component>
+  `,
+  directives: [MyComponent]
 })
 export class AppComponent  { name = 'Angular'; }
--- video.lesson/app/app.module.ts
+++ video.lesson/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {MyComponent} from "./my-component";
+
+const Components = [
+  AppComponent,
+  MyComponent
+];
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ ...Components ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
