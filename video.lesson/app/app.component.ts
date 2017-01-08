43b8b0e49fcfb736b2d446498b3364aac242fc51
Sergey <mr.haiker@mail.ru>
2017-01-08 11:48:15 +0200

    Video Lessons [Chapter 4 : Templates, Events, and Services] Learn *ngIf, *ngSwitch, *ngFor

--- video.lesson/app/app.component.ts
+++ video.lesson/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {MyComponent} from "./my-component";
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>Hello {{name}}</h1>
   <my-component></my-component>
-  `,
-  directives: [MyComponent]
+  <todos></todos>
+  `
 })
 export class AppComponent  { name = 'Angular'; }
--- video.lesson/app/app.module.ts
+++ video.lesson/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import {MyComponent} from "./my-component";
+import {TodosComponent} from "./todo-component";
 
 const Components = [
   AppComponent,
-  MyComponent
+  MyComponent,
+  TodosComponent
 ];
 
 @NgModule({
